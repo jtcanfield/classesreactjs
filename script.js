e7d693009415286c0fe8f2fb9b9457597cb490ca
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-12 16:34:35 -0400

    finish trailblazer

--- script.js
+++ script.js
@@ -161,27 +161,23 @@ console.log(miataRf.customerBuild("Black", [" Hid Headlights"," Sports Suspensio
 
 // Write your 'trailBlazer' instance below:
 // Write your code below:
-model (Trail Blazer), color (blue), trim (Sport), enginesize (8), hitch (true), bed (standard), navigation (true), doors (2)
-constructor(model, color, enginesize, hitch, bed, navigation) {
-let miataRf = new Sport("Miata-RF", "Red", "Manual", "Grand Touring", "Premium", "Premium", "Leather", "Hard Top");
-
-
+                // trim (Sport), doors (2)
+// constructor(model, color, enginesize, hitch, bed, navigation) {
+let trailBlazer = new Truck("Trail Blazer", "Blue", 8, true, "Standard", true);
 
 
 // Print trailBlazer. It should have all the above properties. Plus, the extended warranty.
 // Write your code below:
-
-
+console.log(trailBlazer);
 
 
 // Print trailBlazer, calling massBuid(). It should build 35000 trucks.
 // It should print: "Building 35000 blue Sport Trail Blazer's."
 // Wrint your code below:
-
-
-
+console.log(trailBlazer.massBuild(35000));
 
 
 // Print trailBlazer, calling customerBuild(). It should build a red Trail Blazer with the following options, as an array: seat warmers, tinted windows, fog lamps.
 // It should print: "Building one red Sport Trail Blazer with the following options: seat warmers, tinted windows, fog lamps"
 // Write your code below:
+console.log(trailBlazer.customerBuild("Red"), [" Seat Warmers"," Tinted Windows"," Fog Lamps"]);
